976d8885ed76e276cecc6f28303d826ea816c641
zhangxiaoxu@100tal.com <zhangxiaoxu@100tal.com>
2019-11-19 12:05:41 +0800

    add testcase

--- stackError_test.go
+++ stackError_test.go
@@ -2,23 +2,24 @@
 package stackerror
 
 import (
-	"strings"
+	"fmt"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 
-	err := New("test error")
-	if err == nil || err.GetMsg() != "test error" || len(err.GetStacks()) < 1 {
+	rawError := New("test error")
+
+	stackErr := NewParent("parent msg", rawError)
+	if stackErr.GetChild() != rawError || stackErr.GetMsg() != "parent msg" || len(stackErr.GetStacks()) < 1 {
 		t.Fail()
 	}
-	parentErr := NewParent("parent msg", err)
-	if parentErr.GetChild() != err {
+
+	formatStr := fmt.Sprint(stackErr)
+	if formatStr == "" {
 		t.Fail()
 	}
-	parentStr := parentErr.Error()
-	if parentStr == parentErr.GetMsg() || strings.Index(parentStr, err.GetMsg()) == -1 {
-		t.Fail()
-	}
+	//rawError := errors.New("newerror")
+	//stackParent := NewParent("parentStack", rawError)
 
 }
